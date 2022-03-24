f70a01ef338b022e558b1424296b4e853ebcfd08
8977769080 <51908261+8977769080@users.noreply.github.com>
2022-03-24 11:16:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.94.0"
-    }
-  }
-}
-provider "azurerm" {
-  features {  
-  }
-}
+
 resource "azurerm_resource_group" "example" {
   name     = var.name
   location = var.location
